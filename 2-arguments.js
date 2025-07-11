6c4ad89d24dac056ead3d1973456b63349c5817d
Shanice Nzilani Mwanza <cleopaskioko7@gmail.com>
2025-07-11 01:50:07 -0700

    Arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,6 @@
 const args = process.argv.slice(2);
 if (args.length === 0) {
-conso
-le.log('No argument');
+console.log('No argument');
 } else if (args.length === 1) {
 console.log('Argument found');
 } else {
